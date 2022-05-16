6eaf12ac7ecefde52f54b6d75cf13b14135c8c55
unknown <kkong3456@gmail.com>
2022-05-17 07:11:45 +0900

    second commit

--- lib/data/api.dart
+++ lib/data/api.dart
@@ -3,18 +3,14 @@ import 'dart:convert';
 import 'mise.dart';
 import 'package:http/http.dart' as http;
 
-// https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=kndRcYCKDAPFxA5su9ykdBZy%2F%2BM4mDWXigbul53fmnrJIfS1qiJAVJskQiGCEk469r0tOpwni%2BLYKVAxPTNmlQ%3D%3D&returnType=xml&numOfRows=100&pageNo=1&stationName=%EC%A2%85%EB%A1%9C%EA%B5%AC&dataTerm=DAILY&ver=1.0
-
 class MiseApi {
-  final BASE_URL = "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc";
+  final BASE_URL = "http://apis.data.go.kr/B552584/ArpltnInforInqireSvc";
   final String key =
       "kndRcYCKDAPFxA5su9ykdBZy%2F%2BM4mDWXigbul53fmnrJIfS1qiJAVJskQiGCEk469r0tOpwni%2BLYKVAxPTNmlQ%3D%3D";
 
   Future<List<Mise>> getMiseData(String stationName) async {
     String url =
-        // "$BASE_URL/getMsrstnAcctoRltmMesureDnsty?serviceKey=$key&returnType=json&numOfRows=100&pageNo=1&stationName=${Uri.encodeQueryComponent(stationName)}&dataTerm=DAILY&ver=1.0";
-        // "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=kndRcYCKDAPFxA5su9ykdBZy%2F%2BM4mDWXigbul53fmnrJIfS1qiJAVJskQiGCEk469r0tOpwni%2BLYKVAxPTNmlQ%3D%3D&returnType=json&numOfRows=100&pageNo=1&stationName=${Uri.encodeQueryComponent(stationName)}&dataTerm=DAILY&ver=1.0";
-        "https://apis.data.go.kr/B552584/ArpltnInforInqireSvc/getMsrstnAcctoRltmMesureDnsty?serviceKey=kndRcYCKDAPFxA5su9ykdBZy%2F%2BM4mDWXigbul53fmnrJIfS1qiJAVJskQiGCEk469r0tOpwni%2BLYKVAxPTNmlQ%3D%3D&returnType=json&numOfRows=100&pageNo=1&stationName=%EA%B5%AC%EB%A1%9C%EA%B5%AC&dataTerm=DAILY&ver=1.0";
+        "$BASE_URL/getMsrstnAcctoRltmMesureDnsty?serviceKey=$key&returnType=json&numOfRows=100&pageNo=1&stationName=${Uri.encodeQueryComponent(stationName)}&dataTerm=DAILY&ver=1.0";
     final response = await http.get(url);
 
     print(utf8.decode(response.bodyBytes));
--- lib/main.dart
+++ lib/main.dart
@@ -30,30 +30,117 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List<Color> colors = const [
+    Color(0xFF0077C2),
+    Color(0xFF009BA9),
+    Color(0xFFFe6300),
+    Color(0xFFD80019)
+  ];
+
+  List<String> icon = [
+    "assets/img/happy.png",
+    "assets/img/sad.png",
+    "assets/img/bad.png",
+    "assets/img/angry.png"
+  ];
+
+  List<String> status = ["좋음", "보통", "나쁨", "매우나쁨"];
+  String stationName = "성산읍";
+  List<Mise> data = [];
+
+  int getStatus(Mise mise) {
+    if (mise.pm10 > 150) {
+      return 3;
+    } else if (mise.pm10 > 80) {
+      return 2;
+    } else if (mise.pm10 > 30) {
+      return 1;
+    }
+    return 0;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getMiseData();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("xxx"),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[],
-        ),
-      ),
+      body: getPage(),
       floatingActionButton: FloatingActionButton(
-        onPressed: () async {
-          MiseApi api = new MiseApi();
-          print('xxxxxx');
-          List<Mise> data = await api.getMiseData("구로구");
-
-          for (final d in data) {
-            print("${d.dataTime} ${d.pm10}");
-          }
-        },
+        onPressed: () {},
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  Widget getPage() {
+    if (data.isEmpty) {
+      return Container();
+    }
+
+    int _status = getStatus(data.first);
+
+    return Container(
+        color: colors[_status],
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Container(
+              height: 60,
+            ),
+            const Text("현재위치",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.white,
+                    fontWeight: FontWeight.bold)),
+            Container(
+              height: 12,
+            ),
+            Text(
+              "[$stationName]",
+              textAlign: TextAlign.center,
+              style: const TextStyle(fontSize: 16, color: Colors.white),
+            ),
+            Container(
+              height: 60,
+            ),
+            Container(
+              width: 200,
+              height: 200,
+              child: Image.asset(
+                icon[_status],
+                fit: BoxFit.contain,
+              ),
+            ),
+            Container(
+              height: 60,
+            ),
+            Text(status[_status],
+                textAlign: TextAlign.center,
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                )),
+            Container(
+              height: 20,
+            ),
+            Text("통합 환경 대기 지수 : ${data.first.khai}",
+                textAlign: TextAlign.center,
+                style: const TextStyle(fontSize: 14, color: Colors.white)),
+          ],
+        ));
+  }
+
+  void getMiseData() async {
+    MiseApi api = MiseApi();
+    data = await api.getMiseData(stationName);
+    data.removeWhere((m) => m.pm10 == 0);
+
+    setState(() {});
   }
 }
